db57bd5042c6ac3662fd66ef91f20a34cb782172
ErvinGU <ervin.garay+1@gmail.com>
2025-06-01 21:07:53 +0000

    word_count

---
+++ word_count.py
@@ -0,0 +1,27 @@
+import sys
+
+
+if len(sys.argv) != 2:
+    print("<nombre_archivo>")
+    sys.exit(1)
+
+nombre_archivo = sys.argv[1]
+
+try:
+    with open(nombre_archivo, "r", encoding="utf-8") as file:
+        texto = file.read()
+    
+ 
+    caracteres_distintos = set(texto)
+    num_caracteres_distintos = len(caracteres_distintos)
+    
+    palabras = texto.split()
+    palabras_distintas = set(palabras)  
+    num_palabras_distintas = len(palabras_distintas)
+
+    
+    print(f"El número de caracteres distintos es: {num_caracteres_distintos}")
+    print(f"El número de palabras distintas es: {num_palabras_distintas}")
+
+except FileNotFoundError:
+    print(f"Error: El archivo '{nombre_archivo}' no fue encontrado.")
